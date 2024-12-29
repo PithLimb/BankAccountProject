ff4ff728d152736086105bd3f8d10a5b29515a99
Parth Limbachia <partlimb39@gmail.com>
2024-12-29 21:55:46 +0000

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -4,16 +4,28 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-public class Bank {
-    private BankAccount account = null; // Currently logged-in account
 
-    // Constructor
+// Bank class - simple implementation of a bank, with a list of bank accounts and
+// a current account that we are logged in to.
+
+// This class contains one method ('login') which you need to complete first
+// to make the basic ATM work.
+
+// If you choose the ATM for your project, you should make other modifications to 
+// the system yourself.
+public class Bank {
+    // Instance variables containing the bank information
+    private BankAccount account = null; // Currently logged-in account ('null' if no one is logged in)
+
+    // Constructor method - this provides a couple of example bank accounts to work with
     public Bank() {
         Debug.trace("Bank::<constructor>");
         // No need to initialize in-memory accounts
     }
 
-    // Login method
+    // Check whether the current saved account and password correspond to 
+    // an actual bank account, and if so login to it (by setting 'account' to it)
+    // and return true. Otherwise, reset the account to null and return false
     public boolean login(int newAccNumber, int newAccPasswd) {
         Debug.trace("Bank::login: accNumber = " + newAccNumber);
         logout(); // Logout any previous account
@@ -31,7 +43,7 @@ public class Bank {
         }
     }
 
-    // Logout method
+    // Reset the bank to a 'logged out' state
     public void logout() {
         if (loggedIn()) {
             Debug.trace("Bank::logout: logging out, accNumber = " + account.getAccNumber());
@@ -39,12 +51,13 @@ public class Bank {
         }
     }
 
-    // Check if logged in
+    // Test whether the bank is logged in to an account or not
     public boolean loggedIn() {
         return account != null;
     }
 
-    // Deposit money
+    // Try to deposit money into the account (by calling the deposit method on the 
+    // BankAccount object)
     public boolean deposit(double amount) {
         if (loggedIn()) {
             return account.deposit(amount);
@@ -53,7 +66,8 @@ public class Bank {
         }
     }
 
-    // Withdraw money
+    // Try to withdraw money into the account (by calling the withdraw method on the 
+    // BankAccount object)
     public boolean withdraw(double amount) {
         if (loggedIn()) {
             return account.withdraw(amount);
@@ -62,12 +76,13 @@ public class Bank {
         }
     }
 
-    // Get balance
+    // Get the account balance (by calling the balance method on the 
+    // BankAccount object)
     public double getBalance() {
         if (loggedIn()) {
             return account.getBalance();
         } else {
-            return -1; // Indicator of an error
+            return -1; // Use -1 as an indicator of an error
         }
     }
 }
