07b43e4c28e02f2e402121f9f0c952c320f74932
Parth Limbachia <partlimb39@gmail.com>
2024-12-29 21:58:10 +0000

    Update Model.java

--- Model.java
+++ Model.java
@@ -1,38 +1,43 @@
 package application;
 
-//The model represents all the actual content and functionality of the app
-//For the ATM, it keeps track of the information shown in the display
-//(the title and two message boxes), and the interaction with the bank, executes
-//commands provided by the controller and tells the view to update when
-//something changes
+// The model represents all the actual content and functionality of the app
+// For the ATM, it keeps track of the information shown in the display
+// (the title and two message boxes), and the interaction with the bank, executes
+// commands provided by the controller and tells the view to update when
+// something changes
 public class Model {
-    // ATM states
+    // the ATM model is always in one of three states - waiting for an account number, 
+    // waiting for a password, or logged in and processing account requests. 
+    // We use string values to represent each state:
+    // (the word 'final' tells Java we won't ever change the values of these variables)
     final String ACCOUNT_NO = "account_no";
     final String PASSWORD = "password";
     final String LOGGED_IN = "logged_in";
 
-    // Model variables
+    // variables representing the ATM model
     String state = ACCOUNT_NO;      // Current state
-    int  number = 0;                // Current number input
-    Bank  bank = null;              // Bank object
-    int accNumber = -1;             // Entered account number
-    int accPasswd = -1;             // Entered password
-    // Display variables
-    String title = "Bank ATM";      // Title message
-    String display1 = null;         // Message 1
-    String display2 = null;         // Message 2
+    int  number = 0;                // current number displayed in GUI (as a number, not a string)
+    Bank  bank = null;              // The ATM talks to a bank, represented by the Bank object
+    int accNumber = -1;             // Account number typed in
+    int accPasswd = -1;             // Password typed in
+    // These three are what are shown on the View display
+    String title = "Bank ATM";      // The contents of the title message
+    String display1 = null;         // The contents of the Message 1 box (a single line)
+    String display2 = null;         // The contents of the Message 2 box (may be multiple lines)
 
-    // MVC components
+    // The other parts of the model-view-controller setup
     public View view;
     public Controller controller;
 
-    // Constructor
+    // Model constructor - we pass it a Bank object representing the bank we want to talk to
     public Model(Bank b) {
         Debug.trace("Model::<constructor>");          
         bank = b;
     }
 
-    // Initialize ATM state
+    // Initialising the ATM (or resetting after an error or logout)
+    // set state to ACCOUNT_NO, number to zero, and display message 
+    // provided as argument and standard instruction message
     public void initialise(String message) {
         setState(ACCOUNT_NO);
         number = 0;
@@ -41,7 +46,8 @@ public class Model {
                     "Followed by \"Ent\"";
     }
 
-    // Change state with debugging
+    // use this method to change state - mainly so we print a debugging message whenever 
+    // the state changes
     public void setState(String newState) {
         if (!state.equals(newState)) {
             String oldState = state;
@@ -50,25 +56,31 @@ public class Model {
         }
     }
 
-    // Process number key
+    // process a number key (the key is specified by the label argument)
     public void processNumber(String label) {
         char c = label.charAt(0);
         number = number * 10 + (c - '0'); // Build number 
         display1 = "" + number;
-        display();  // Update GUI
+        display();  // update the GUI
     }
 
-    // Process Clear button
+    // process the Clear button - reset the number (and number display string)
     public void processClear() {
         number = 0;
         display1 = "";
-        display();  // Update GUI
+        display();  // update the GUI
     }
 
-    // Process Enter button
+    // process the Enter button
+    // this is the most complex operation - the Enter key causes the ATM to change state
+    // from account number, to password, to logged_in and back to account number
+    // (when you log out)
     public void processEnter() {
         switch (state) {
             case ACCOUNT_NO:
+                // we were waiting for a complete account number - save the number we have
+                // reset the tyed in number to 0 and change to the state where we are expecting 
+                // a password
                 accNumber = number;
                 number = 0;
                 setState(PASSWORD);
@@ -77,6 +89,9 @@ public class Model {
                            "Followed by \"Ent\"";
                 break;
             case PASSWORD:
+                // we were waiting for a password - save the number we have as the password
+                // and then contact the bank with accNumber and accPasswd to try and login to
+                // an account
                 accPasswd = number;
                 number = 0;
                 display1 = "";
@@ -90,12 +105,13 @@ public class Model {
                 break;
             case LOGGED_IN:     
             default: 
-                // Do nothing in any other state (ie logged in)
+                // do nothing in any other state (ie logged in)
         }  
-        display();  // Update GUI
+        display();  // update the GUI
     }
 
-    // Process Withdraw button
+    // Withdraw button - check we are logged in and if so try and withdraw some money from
+    // the bank (number is the amount showing in the interface display)
     public void processWithdraw() {
         if (state.equals(LOGGED_IN)) {            
             if (bank.withdraw(number)) {
@@ -108,10 +124,11 @@ public class Model {
         } else {
             initialise("You are not logged in");
         }
-        display();  // Update GUI
+        display();  // update the GUI
     }
 
-    // Process Deposit button
+    // Deposit button - check we are logged in and if so try and deposit some money into
+    // the bank (number is the amount showing in the interface display)
     public void processDeposit() {
         if (state.equals(LOGGED_IN)) {
             if (bank.deposit(number)) {
@@ -124,10 +141,11 @@ public class Model {
         } else {
             initialise("You are not logged in");
         }
-        display();  // Update GUI
+        display();  // update the GUI
     }
 
-    // Process Balance button
+    // Balance button - check we are logged in and if so access the current balance
+    // and display it
     public void processBalance() {
         if (state.equals(LOGGED_IN)) {
             double balance = bank.getBalance();
@@ -137,10 +155,10 @@ public class Model {
         } else {
             initialise("You are not logged in");
         }
-        display();  // Update GUI
+        display();  // update the GUI
     }
 
-    // Process Finish (Logout) button
+    // Finish button - check we are logged in and if so log out
     public void processFinish() {
         if (state.equals(LOGGED_IN)) {
             setState(ACCOUNT_NO);
@@ -150,17 +168,18 @@ public class Model {
         } else {
             initialise("You are not logged in");
         }
-        display();  // Update GUI
+        display();  // update the GUI
     }
 
-    // Process Unknown Key
+    // Any other key results in an error message and a reset of the GUI
     public void processUnknownKey(String action) {
         Debug.trace("Model::processUnknownKey: unknown button \"" + action + "\", re-initialising");
         initialise("Invalid command");
         display();
     }
 
-    // Update View
+    // This is where the Model talks to the View, by calling the View's update method
+    // The view will call back to the model to get new information to display on the screen
     public void display() {
         Debug.trace("Model::display");
         view.update();
