df8138b32e83677e574a419c32bc8a9212e64829
Parth Limbachia <partlimb39@gmail.com>
2024-12-29 21:56:09 +0000

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -5,111 +5,109 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+// BankAccount class
+// This class has instance variables for the account number, password and balance, and methods
+// to withdraw, deposit, check balance etc.
+// This class contains methods which you need to complete to make the basic ATM work.
+// Tutors can help you get this part working in lab sessions. 
+// If you choose the ATM for your project, you should make other modifications to 
+// the system yourself.
 public class BankAccount {
-    private int accNumber;
-    private int accPasswd;
-    private double balance;
+   private int accNumber;
+   private int accPasswd; 
+   private double balance;
 
-    // Database connection details
-    // Use double backslashes in Java strings
-    private static final String CONNECTION_URL = 
-	        "jdbc:sqlserver://V\\SQLEXPRESS;databaseName=ATMDB;integratedSecurity=true;trustServerCertificate=true;";
+   // Database connection details
+   // Use double backslashes in Java strings
+   private static final String CONNECTION_URL = 
+           "jdbc:sqlserver://V\\SQLEXPRESS;databaseName=ATMDB;integratedSecurity=true;trustServerCertificate=true;";
 
-    // Static block to load the JDBC driver
-    static {
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            Debug.trace("JDBC Driver Registered!");
-        } catch (ClassNotFoundException e) {
-            Debug.trace("JDBC Driver not found: " + e.getMessage());
-        }
-    }
+   // Static block to load the JDBC driver
+   static {
+       try {
+           Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+           Debug.trace("JDBC Driver Registered!");
+       } catch (ClassNotFoundException e) {
+           Debug.trace("JDBC Driver not found: " + e.getMessage());
+       }
+   }
 
-    // Constructor
-    public BankAccount(int a, int p, double b) {
-        this.accNumber = a;
-        this.accPasswd = p;
-        this.balance = b;
-    }
+   // Constructor
+   public BankAccount(int a, int p, double b) {
+       this.accNumber = a;
+       this.accPasswd = p;
+       this.balance = b;
+   }
 
-    // Getters
-    public int getAccNumber() {
-        return accNumber;
-    }
+   // Getters
+   public int getAccNumber() {
+       return accNumber;
+   }
 
-    public int getAccPasswd() {
-        return accPasswd;
-    }
+   public int getAccPasswd() {
+       return accPasswd;
+   }
 
-    public double getBalance() {
-        return balance;
-    }
+   // Return the current balance in the account
+   public double getBalance() {
+       return balance;
+   }
 
-    // Withdraw money from the account
-    public boolean withdraw(double amount) {
-        Debug.trace("BankAccount::withdraw: amount = " + amount);
+   // Withdraw money from the account. Return true if successful, or 
+   // false if the amount is negative, or less than the amount in the account
+   public boolean withdraw(double amount) {
+       Debug.trace("BankAccount::withdraw: amount = " + amount);
+       if (amount > 0 && (balance - amount) >= -500) { // Allow overdraft up to -500
+           balance -= amount;
+           updateBalanceInDB();
+           return true;
+       }
+       return false;
+   }
 
-        if (amount > 0 && (balance - amount) >= -500) { // Allow overdraft up to -500
-            balance -= amount;
-            updateBalanceInDB();
-            return true;
-        }
-        return false;
-    }
+   // Deposit the amount of money into the account. Return true if successful,
+   // or false if the amount is negative
+   public boolean deposit(double amount) {
+       Debug.trace("BankAccount::deposit: amount = " + amount);
+       if (amount > 0) {
+           balance += amount;
+           updateBalanceInDB();
+           return true;
+       }
+       return false;
+   }
 
-    // Deposit money into the account
-    public boolean deposit(double amount) {
-        Debug.trace("BankAccount::deposit: amount = " + amount);
+   // Update balance in the database  
+   private void updateBalanceInDB() {
+       String updateSQL = "UPDATE BankAccounts SET Balance = ? WHERE AccountID = ?";
+       try (Connection con = DriverManager.getConnection(CONNECTION_URL);
+            PreparedStatement ps = con.prepareStatement(updateSQL)) {
+           ps.setDouble(1, balance);
+           ps.setInt(2, accNumber);
+           ps.executeUpdate();
+           Debug.trace("BankAccount::updateBalanceInDB: Account " + accNumber + " new balance " + balance);
+       } catch (SQLException e) {
+           Debug.trace("BankAccount::updateBalanceInDB: SQLException: " + e.getMessage());
+       }
+   }
 
-        if (amount > 0) {
-            balance += amount;
-            updateBalanceInDB();
-            return true;
-        }
-        return false;
-    }
-
-    // Update balance in the database
-    private void updateBalanceInDB() {
-        String updateSQL = "UPDATE BankAccounts SET Balance = ? WHERE AccountID = ?";
-
-        try (Connection con = DriverManager.getConnection(CONNECTION_URL);
-             PreparedStatement ps = con.prepareStatement(updateSQL)) {
-
-            ps.setDouble(1, balance);
-            ps.setInt(2, accNumber);
-            ps.executeUpdate();
-
-            Debug.trace("BankAccount::updateBalanceInDB: Account " + accNumber + " new balance " + balance);
-
-        } catch (SQLException e) {
-            Debug.trace("BankAccount::updateBalanceInDB: SQLException: " + e.getMessage());
-        }
-    }
-
-    // Static method to fetch a BankAccount from the database
-    public static BankAccount fetchAccount(int accNumber, int accPasswd) {
-        String selectSQL = "SELECT * FROM BankAccounts WHERE AccountID = ? AND Password = ?";
-
-        try (Connection con = DriverManager.getConnection(CONNECTION_URL);
-             PreparedStatement ps = con.prepareStatement(selectSQL)) {
-
-            ps.setInt(1, accNumber);
-            ps.setInt(2, accPasswd);
-
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-                double bal = rs.getDouble("Balance");
-                Debug.trace("BankAccount::fetchAccount: Found AccountID " + accNumber + " with balance " + bal);
-                return new BankAccount(accNumber, accPasswd, bal);
-            }
-
-        } catch (SQLException e) {
-            Debug.trace("BankAccount::fetchAccount: SQLException: " + e.getMessage());
-        }
-
-        Debug.trace("BankAccount::fetchAccount: Account not found.");
-        return null;
-    }
+   // Static method to fetch a BankAccount from the database
+   public static BankAccount fetchAccount(int accNumber, int accPasswd) {
+       String selectSQL = "SELECT * FROM BankAccounts WHERE AccountID = ? AND Password = ?";
+       try (Connection con = DriverManager.getConnection(CONNECTION_URL);
+            PreparedStatement ps = con.prepareStatement(selectSQL)) {
+           ps.setInt(1, accNumber);
+           ps.setInt(2, accPasswd);
+           ResultSet rs = ps.executeQuery();
+           if (rs.next()) {
+               double bal = rs.getDouble("Balance");
+               Debug.trace("BankAccount::fetchAccount: Found AccountID " + accNumber + " with balance " + bal);
+               return new BankAccount(accNumber, accPasswd, bal);
+           }
+       } catch (SQLException e) {
+           Debug.trace("BankAccount::fetchAccount: SQLException: " + e.getMessage());
+       }
+       Debug.trace("BankAccount::fetchAccount: Account not found.");
+       return null;
+   }
 }
